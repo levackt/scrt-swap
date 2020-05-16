7449fbefaef974532a7379e28ecba4411febf73e
Taariq Levack <taariql@gmail.com>
2020-05-16 11:13:46 +0200

    enigma style base

--- client/src/theme.js
+++ client/src/theme.js
@@ -1,33 +1,11 @@
 import { createMuiTheme} from '@material-ui/core/styles';
 
-import green from "@material-ui/core/colors/green";
-import purple from "@material-ui/core/colors/purple";
-
-const primaryGreen = green[500];
-const accentGreen = green.A200;
-const darkGreen = green[900];
-const primaryPurple = purple[500];
-const accentPurple = purple.A200;
-const darkPurple = purple[900];
-
-export const overridings = {
-  name: 'Dark Theme',
-  palette: {
-    primary: {
-      light: accentPurple,
-      main: primaryPurple,
-      dark: darkPurple,
-      contrastText: "#fff"
+const theme = createMuiTheme({
+  typography: {
+    button: {
+      fontSize: '2.5rem',
     },
-    type: "dark",
-    secondary: {
-      light: accentGreen,
-      main: primaryGreen,
-      dark: darkGreen,
-      contrastText: "#fff"
-    }
-  }
-};
-const theme = createMuiTheme(overridings);
+  },
+});
 
 export default theme
